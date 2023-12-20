6663d284f68093692211cccd70c7b87828dda0ea
Nisarg <202003030@daiict.ac.in>
2023-12-20 17:37:44 +0530

    Update connectivity.py

--- eeg_analysis/connectivity.py
+++ eeg_analysis/connectivity.py
@@ -1,16 +1,22 @@
 import os
+import shutil
 import mne
 from scripts.synclike import Synchronization
 
 
-band = input("Enter the path to the data: ")
+band = input("Enter the band: ")
 path = os.path.join(os.getcwd(), "filtered", band + "_raw.fif.gz")
 data = mne.io.read_raw_fif(path, preload=True)
 
 params = {"delta": {"m": 25, "lag": 41, "w1": 1968, "w2": 2967}, "theta": {"m": 7, "lag": 20, "w1": 240, "w2": 1239}, "alpha": {
     "m": 6, "lag": 12, "w1": 120, "w2": 1119}, "beta": {"m": 8, "lag": 5, "w1": 70, "w2": 1069}, "gamma": {"m": 6, "lag": 3, "w1": 30, "w2": 1029}}
 
-sync = Synchronization(data, *params[band])
+sync = Synchronization(data.get_data(), *params[band])
 
-connectivity = sync.get_connectivity()
+connectivity = sync.connectivity()
 print(connectivity.shape)
+
+shutil.move("connectivity.mat", band)
+shutil.move("sync", band)
+
+print("DONE!")
